d5f1f4d3929aedd1aaabf73837d5182ddf27a8e5
Kristian Lundkvist <kristian.lundkvist@gmail.com>
2011-09-08 13:04:10 +0200

    This should fix all the mess created be trying to merge WikiSearch with master.

--- prepare.sh
+++ prepare.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 wget http://www.jibble.org/files/pircbot-1.5.0.zip
 unzip pircbot-1.5.0.zip
+wget http://jwpl.googlecode.com/files/jwpl_v05b.jar
 wget http://www.jibble.org/files/JMegaHal.jar
-mkdir lib/
+mkdir lib
 mv -v pircbot-1.5.0/pircbot.jar lib/
+mv -v jwpl_v05b.jar lib/
 mv -v JMegaHal.jar lib/
 chmod 755 -v lib/*
 rm -v pircbot-1.5.0.zip
-rm -rv pircbot-1.5.0
-rm LICENCE.txt
-rm CHANGELOG.txt
--- src/se/bthstudent/sis/afk/GLaDOS/GenericUtilityProcessor.java
+++ src/se/bthstudent/sis/afk/GLaDOS/GenericUtilityProcessor.java
@@ -16,6 +16,24 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+/*
+    Copyright (C) 2011  Kristian 'Bobby' Lundkvist, Niclas 'Prosten' Bj�rner
+
+	This file is a part of GLaDOS
+
+    This GLaDOS is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 package se.bthstudent.sis.afk.GLaDOS;
 
@@ -43,7 +61,7 @@ public class GenericUtilityProcessor implements Serializable{
 	 * Constructor
 	 */
 	public GenericUtilityProcessor(){}
-
+	
 	/**
 	 * Saves GLaDOS to a file for safety (to be ready when the shit/neurotoxin hits the fan.
 	 * @param temp GLaDOS
@@ -51,35 +69,35 @@ public class GenericUtilityProcessor implements Serializable{
 	public void saveGLaDOStoFile(Object temp){
 		try {
 			ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("GLaDOS.backup"));
-
+			
 			out.writeObject(temp);
-
+			
 			out.close();
-		}
+		} 
 		catch (FileNotFoundException e) {
 			e.printStackTrace();
-		}
+		} 
 		catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-
+	
 	/**
 	 * Loads GLaDOS from backup.
 	 * @return GLaDOS
 	 */
 	public Object loadGLaDOSfromFile(){
 		Object temp = new Object();
-
+		
 		try{
 			ObjectInputStream in = new ObjectInputStream(new FileInputStream("GLaDOS.backup"));
-
+			
 			try {
 				temp = (GLaDOS)in.readObject();
 			} catch (ClassNotFoundException e) {
 				e.printStackTrace();
 			}
-
+			
 			in.close();
 		} catch (FileNotFoundException e) {
 			System.err
@@ -94,7 +112,7 @@ public class GenericUtilityProcessor implements Serializable{
 
 	/**
 	 * Backup test subjects to file (TestSubjects.backup).
-	 *
+	 * 
 	 * @param temp
 	 *            Test subjects
 	 */
@@ -115,7 +133,7 @@ public class GenericUtilityProcessor implements Serializable{
 
 	/**
 	 * Load test subjects from file (TestSubjects.backup).
-	 *
+	 * 
 	 * @return Object Test Subjects */
 	public Object loadTestSubjectsFromFile() {
 		Object temp = new Object();
@@ -136,7 +154,7 @@ public class GenericUtilityProcessor implements Serializable{
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
+		
 		return temp;
 	}
 
