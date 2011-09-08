f29594988def693f446e943442b1bf25b18492bd
Kristian Lundkvist <kristian.lundkvist@gmail.com>
2011-09-08 12:54:33 +0200

    Revert "Fixed some problems with the missing libraries, also fixed some smaller"
    This reverts commit db0a44af085b568e2ecb12013d0728195dece56c.
    
    Conflicts:
    
    	src/se/bthstudent/sis/afk/GLaDOS/GenericUtilityProcessor.java

--- src/se/bthstudent/sis/afk/GLaDOS/GenericUtilityProcessor.java
+++ src/se/bthstudent/sis/afk/GLaDOS/GenericUtilityProcessor.java
@@ -1,5 +1,27 @@
 /*
+<<<<<<< HEAD
     Copyright (C) 2011  Kristian 'Bobby' Lundkvist, Niclas 'Prosten' BjÃ¶rner
+=======
+    Copyright (C) 2011  Kristian 'Bobby' Lundkvist, Niclas 'Prosten' Bjï¿½rner
+
+	This file is a part of GLaDOS
+
+    This GLaDOS is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*
+    Copyright (C) 2011  Kristian 'Bobby' Lundkvist, Niclas 'Prosten' Björner
+>>>>>>> parent of db0a44a... Fixed some problems with the missing libraries, also fixed some smaller
 
 	This file is a part of GLaDOS
 
